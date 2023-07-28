2d63f42ab8cf90af804f02a51a8218db7f150b26
David Gethers <dgethers.jr@gmail.com>
2023-07-28 10:50:28 -0400

    Add abstraction for game

--- test_tic_tac_toe.py
+++ test_tic_tac_toe.py
@@ -1,5 +1,5 @@
 import unittest
-from tic_tac_toe import TicTacToeBoard
+from tic_tac_toe import TicTacToeBoard, TicTacToeGame
 from tic_tac_toe import TicTacToeSelection
 import os
 
@@ -14,7 +14,7 @@ class TicTacToeTest(unittest.TestCase):
 
     def test_add_O_move_to_board(self):
         tic_tac_toe_board = TicTacToeBoard()
-        tic_tac_toe_board.move(0, 0, TicTacToeSelection.O)
+        tic_tac_toe_board.add_move(0, 0, TicTacToeSelection.O)
         expected = f"[O][ ][ ]{os.linesep}---------{os.linesep}[ ][ ][ ]{os.linesep}---------{os.linesep}[ ][ ][ ]{os.linesep}"
         actual = str(tic_tac_toe_board)
 
@@ -22,7 +22,7 @@ class TicTacToeTest(unittest.TestCase):
 
     def test_add_X_move_to_board(self):
         tic_tac_toe_board = TicTacToeBoard()
-        tic_tac_toe_board.move(0, 0, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(0, 0, TicTacToeSelection.X)
         expected = f"[X][ ][ ]{os.linesep}---------{os.linesep}[ ][ ][ ]{os.linesep}---------{os.linesep}[ ][ ][ ]{os.linesep}"
         actual = str(tic_tac_toe_board)
 
@@ -30,8 +30,8 @@ class TicTacToeTest(unittest.TestCase):
 
     def test_add_selection_to_cell_that_already_has_value(self):
         tic_tac_toe_board = TicTacToeBoard()
-        tic_tac_toe_board.move(0, 0, TicTacToeSelection.X)
-        tic_tac_toe_board.move(0, 0, TicTacToeSelection.O)
+        tic_tac_toe_board.add_move(0, 0, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(0, 0, TicTacToeSelection.O)
         expected = f"[X][ ][ ]{os.linesep}---------{os.linesep}[ ][ ][ ]{os.linesep}---------{os.linesep}[ ][ ][ ]{os.linesep}"
         actual = str(tic_tac_toe_board)
 
@@ -39,60 +39,75 @@ class TicTacToeTest(unittest.TestCase):
 
     def test_X_wins_the_row(self):
         tic_tac_toe_board = TicTacToeBoard()
-        tic_tac_toe_board.move(0, 0, TicTacToeSelection.X)
-        tic_tac_toe_board.move(0, 1, TicTacToeSelection.X)
-        tic_tac_toe_board.move(0, 2, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(0, 0, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(0, 1, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(0, 2, TicTacToeSelection.X)
         actual = tic_tac_toe_board.validate_win()
 
         self.assertEqual(actual, TicTacToeSelection.X)
 
     def test_O_wins_the_column(self):
         tic_tac_toe_board = TicTacToeBoard()
-        tic_tac_toe_board.move(0, 0, TicTacToeSelection.O)
-        tic_tac_toe_board.move(1, 0, TicTacToeSelection.O)
-        tic_tac_toe_board.move(2, 0, TicTacToeSelection.O)
+        tic_tac_toe_board.add_move(0, 0, TicTacToeSelection.O)
+        tic_tac_toe_board.add_move(1, 0, TicTacToeSelection.O)
+        tic_tac_toe_board.add_move(2, 0, TicTacToeSelection.O)
         actual = tic_tac_toe_board.validate_win()
 
         self.assertEqual(actual, TicTacToeSelection.O)
 
     def test_X_wins_the_diagonal(self):
         tic_tac_toe_board = TicTacToeBoard()
-        tic_tac_toe_board.move(0, 0, TicTacToeSelection.X)
-        tic_tac_toe_board.move(1, 1, TicTacToeSelection.X)
-        tic_tac_toe_board.move(2, 2, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(0, 0, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(1, 1, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(2, 2, TicTacToeSelection.X)
         actual = tic_tac_toe_board.validate_win()
 
         self.assertEqual(actual, TicTacToeSelection.X)
 
     def test_tie_game(self):
         tic_tac_toe_board = TicTacToeBoard()
-        tic_tac_toe_board.move(0, 0, TicTacToeSelection.X)
-        tic_tac_toe_board.move(0, 1, TicTacToeSelection.O)
-        tic_tac_toe_board.move(0, 2, TicTacToeSelection.X)
-        tic_tac_toe_board.move(1, 0, TicTacToeSelection.X)
-        tic_tac_toe_board.move(1, 1, TicTacToeSelection.X)
-        tic_tac_toe_board.move(1, 2, TicTacToeSelection.O)
-        tic_tac_toe_board.move(2, 0, TicTacToeSelection.O)
-        tic_tac_toe_board.move(2, 1, TicTacToeSelection.X)
-        tic_tac_toe_board.move(2, 2, TicTacToeSelection.O)
+        tic_tac_toe_board.add_move(0, 0, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(0, 1, TicTacToeSelection.O)
+        tic_tac_toe_board.add_move(0, 2, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(1, 0, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(1, 1, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(1, 2, TicTacToeSelection.O)
+        tic_tac_toe_board.add_move(2, 0, TicTacToeSelection.O)
+        tic_tac_toe_board.add_move(2, 1, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(2, 2, TicTacToeSelection.O)
         actual = tic_tac_toe_board.validate_tie()
 
         self.assertTrue(actual)
 
     def test_tie_game_when_board_is_not_full(self):
         tic_tac_toe_board = TicTacToeBoard()
-        tic_tac_toe_board.move(0, 0, TicTacToeSelection.X)
-        tic_tac_toe_board.move(0, 1, TicTacToeSelection.O)
-        tic_tac_toe_board.move(0, 2, TicTacToeSelection.X)
-        tic_tac_toe_board.move(1, 0, TicTacToeSelection.X)
-        tic_tac_toe_board.move(1, 2, TicTacToeSelection.O)
-        tic_tac_toe_board.move(2, 0, TicTacToeSelection.O)
-        tic_tac_toe_board.move(2, 1, TicTacToeSelection.X)
-        tic_tac_toe_board.move(2, 2, TicTacToeSelection.O)
+        tic_tac_toe_board.add_move(0, 0, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(0, 1, TicTacToeSelection.O)
+        tic_tac_toe_board.add_move(0, 2, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(1, 0, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(1, 2, TicTacToeSelection.O)
+        tic_tac_toe_board.add_move(2, 0, TicTacToeSelection.O)
+        tic_tac_toe_board.add_move(2, 1, TicTacToeSelection.X)
+        tic_tac_toe_board.add_move(2, 2, TicTacToeSelection.O)
         actual = tic_tac_toe_board.validate_tie()
 
         self.assertIsNone(actual)
 
+    def test_adding_x_using_board_position(self):
+        tic_tac_toe_game = TicTacToeGame()
+        tic_tac_toe_game.make_move("ONE", TicTacToeSelection.X)
+        actual = tic_tac_toe_game.get_current_board_state()
+        expected = f"[X][ ][ ]{os.linesep}---------{os.linesep}[ ][ ][ ]{os.linesep}---------{os.linesep}[ ][ ][ ]{os.linesep}"
+
+        self.assertEqual(actual, expected)
+
+    def test_adding_x_using_board_invalid_position(self):
+        tic_tac_toe_game = TicTacToeGame()
+        tic_tac_toe_game.make_move("FOO", TicTacToeSelection.X)
+        actual = tic_tac_toe_game.get_current_board_state()
+        expected = f"[ ][ ][ ]{os.linesep}---------{os.linesep}[ ][ ][ ]{os.linesep}---------{os.linesep}[ ][ ][ ]{os.linesep}"
+
+        self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main()
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from enum import Enum
 from typing import Optional
 import os
@@ -14,48 +15,48 @@ class TicTacToeSelection(Enum):
 
 class TicTacToeBoard:
     def __init__(self):
-        self._board = [[TicTacToeSelection.EMPTY for _ in range(3)] for _ in range(3)]
+        self._grid = [[TicTacToeSelection.EMPTY for _ in range(3)] for _ in range(3)]
 
     def __str__(self):
         output = ""
-        for i in range(len(self._board)):
-            for j in range(len(self._board[i])):
-                output += f"{self._board[i][j]}"
+        for i in range(len(self._grid)):
+            for j in range(len(self._grid[i])):
+                output += f"{self._grid[i][j]}"
             output += os.linesep
-            if i < len(self._board) - 1:
+            if i < len(self._grid) - 1:
                 output += '-' * 9
                 output += os.linesep
 
         return output
 
-    def move(self, x: int, y: int, selection: TicTacToeSelection):
-        if self._board[x][y] is TicTacToeSelection.EMPTY:
-            self._board[x][y] = selection
+    def add_move(self, x: int, y: int, selection: TicTacToeSelection):
+        if self._grid[x][y] is TicTacToeSelection.EMPTY:
+            self._grid[x][y] = selection
 
     def validate_win(self) -> Optional[TicTacToeSelection]:
-        for row in self._board:
+        for row in self._grid:
             if row.count(row[0]) == len(row) and row[0] is not TicTacToeSelection.EMPTY:
                 return row[0]
 
-        for col in range(len(self._board[0])):
-            col_values = [row[col] for row in self._board]
+        for col in range(len(self._grid[0])):
+            col_values = [row[col] for row in self._grid]
             if col_values.count(col_values[0]) == len(col_values) and col_values[0] is not TicTacToeSelection.EMPTY:
                 return col_values[0]
 
-        if (self._board[0][0] == self._board[1][1] == self._board[2][2] and
-                self._board[0][0] is not TicTacToeSelection.EMPTY):
-            return self._board[0][0]
+        if (self._grid[0][0] == self._grid[1][1] == self._grid[2][2] and
+                self._grid[0][0] is not TicTacToeSelection.EMPTY):
+            return self._grid[0][0]
 
-        if (self._board[0][2] == self._board[1][1] == self._board[2][0] and
-                self._board[0][2] is not TicTacToeSelection.EMPTY):
-            return self._board[0][2]
+        if (self._grid[0][2] == self._grid[1][1] == self._grid[2][0] and
+                self._grid[0][2] is not TicTacToeSelection.EMPTY):
+            return self._grid[0][2]
 
         return None
 
     def validate_tie(self) -> Optional[bool]:
         winner = self.validate_win()
 
-        for row in self._board:
+        for row in self._grid:
             if TicTacToeSelection.EMPTY in row:
                 return None
 
@@ -63,6 +64,31 @@ class TicTacToeBoard:
             return True
 
         return False
+
+
+Cell = namedtuple('Cell', ['x', 'y'])
+
+
+class TicTacToeGame:
+    def __init__(self):
+        self._board = TicTacToeBoard()
+        self._positions = {"ONE": Cell(0, 0),
+                           "TWO": Cell(0, 1),
+                           "THREE": Cell(0, 2),
+                           "FOUR": Cell(1, 0),
+                           "FIVE": Cell(1, 1),
+                           "SIX": Cell(1, 2),
+                           "SEVEN": Cell(2, 0),
+                           "EIGHT": Cell(2, 1),
+                           "NINE": Cell(2, 2)}
+
+    def make_move(self, move: str, selection: TicTacToeSelection):
+        cell = self._positions.get(move)
+        if cell:
+            self._board.add_move(cell.x, cell.y, selection)
+
+    def get_current_board_state(self) -> str:
+        return str(self._board)
 
 
 if __name__ == '__main__':
