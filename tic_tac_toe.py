b84377ad56b5b0c41a70bce16bac631ea2875e03
David Gethers <dgethers.jr@gmail.com>
2023-07-28 12:08:29 -0400

    Minor refactoring of cli interface

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 from enum import Enum
-from typing import Optional
+from typing import Optional, Dict
 import os
 
 
@@ -81,6 +81,7 @@ class TicTacToeGame:
                            "SEVEN": Cell(2, 0),
                            "EIGHT": Cell(2, 1),
                            "NINE": Cell(2, 2)}
+        self._valid_positions = self._positions.keys()
 
     def make_move(self, move: str, selection: TicTacToeSelection):
         cell = self._positions.get(move)
@@ -90,6 +91,63 @@ class TicTacToeGame:
     def get_current_board_state(self) -> str:
         return str(self._board)
 
+    def check_for_winner(self) -> Optional[TicTacToeSelection]:
+        return self._board.validate_win()
+
+    def check_for_tie(self) -> Optional[bool]:
+        return self._board.validate_tie()
+
+    def is_valid_position(self, move: str) -> bool:
+        return move.upper() in self._valid_positions
+
 
 if __name__ == '__main__':
-    pass
+    print("The First Player's name")
+    player_first = input("Please mention your name: ")
+    print("\n")
+
+    print("The Second Player's name")
+    player_second = input("Please mention your name:  ")
+    print("\n")
+
+    current_player = player_first
+    tic_tac_toe_game = TicTacToeGame()
+    players = {player_first: TicTacToeSelection.X, player_second: TicTacToeSelection.O}
+    turn_count = 1
+
+    while True:
+        try:
+            print("TicTacToe game where each player will place a X or O on a 3 x 3 grid")
+            print("Use the number from one to nine to represent where on the grid you would like to put your selection")
+            print("You must spell the numbers, 1 is one and so on and so forth. Case does not matter")
+            print("Adding a selection to a square with a value already will be ignored")
+            print("The player ", current_player, " turn. Now you need to choose your block : ")
+            print(tic_tac_toe_game.get_current_board_state())
+
+            chance = input().upper()
+            if tic_tac_toe_game.is_valid_position(chance) is False:
+                print("This is an Invalid Input!!! Please try again!")
+                continue
+
+            tic_tac_toe_game.make_move(chance, players[current_player])
+
+            winner = tic_tac_toe_game.check_for_winner()
+            if winner:
+                print("The winner is: ", current_player)
+                break
+
+            turn_count += 1
+
+            if current_player is player_first:
+                current_player = player_second
+            else:
+                current_player = player_first
+
+            if turn_count > 9:
+                tie = tic_tac_toe_game.check_for_tie()
+                if tie:
+                    print('Tie game detected')
+
+        except ValueError:
+            print("This is an Invalid Input!!! Please try again!")
+            continue
